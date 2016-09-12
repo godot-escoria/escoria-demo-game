b426debb0e63d03e884e923302f5c83fa1beb2f6
Ariel Manzur <ariel@godotengine.org>
2016-09-12 17:18:42 -0300

    workaround for VisualServer::sync keyword bug in latest godot git

--- device/globals/resource_queue.gd
+++ device/globals/resource_queue.gd
@@ -91,7 +91,7 @@ func is_ready(path):
 func _wait_for_resource(res, path):
 	_unlock("wait_for_resource")
 	while true:
-		VS.sync()
+		VS.call("sync") # workaround because sync is a keyword
 		OS.delay_usec(16000) # wait 1 frame
 		_lock("wait_for_resource")
 		if queue.size() == 0 || queue[0] != res:
