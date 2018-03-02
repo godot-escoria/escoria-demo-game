b539861c4c1c4cab2570417ee9a722eb4a0beb4c
Markus Törnqvist <mjt@fadgames.com>
2018-03-02 16:49:42 +0200

    Fix `camera_set_pos` name

--- device/globals/vm_level.gd
+++ device/globals/vm_level.gd
@@ -193,7 +193,7 @@ func camera_set_target(params):
 		targets.push_back(params[i])
 	vm.camera_set_target(speed, targets)
 
-func camera_set_position(params):
+func camera_set_pos(params):
 	var speed = params[0]
 	var pos = Vector2(params[1], params[2])
 	vm.camera_set_target(speed, pos)
