7f5df6e5092f2ea13b7d932a9056e93cc1925378
Markus Törnqvist <mjt@fadgames.com>
2018-04-18 13:16:25 +0300

    Functional trigger and exit with KinematicBody2D player

---
+++ device/globals/exit.gd
@@ -0,0 +1,22 @@
+extends "res://globals/trigger.gd"
+
+func stopped_at(pos):
+	# TextureRect exits run this code
+	if self is Control and get_global_rect().has_point(pos):
+		run_event("exit_scene")
+
+func body_entered(body):
+	# Entering an exit node runs the `:exit_scene` event
+	if body is preload("res://globals/player.gd"):
+		run_event("exit_scene")
+
+func body_exited(body):
+	# Do nothing when exiting an exit node. Usually this code should not be hit.
+	return
+
+func _ready():
+	add_to_group("exit")
+
+	if not "exit_scene" in event_table:
+		vm.report_errors("exit", [":exit_scene missing for " + self.name])
+
