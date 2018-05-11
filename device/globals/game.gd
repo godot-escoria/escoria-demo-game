4a7f69437d238733e48a9944e164f5f8111ac556
Markus Törnqvist <mjt@fadgames.com>
2018-05-11 13:56:27 +0300

    Shuffle elif inventory to place so tooltips work

--- device/globals/game.gd
+++ device/globals/game.gd
@@ -49,7 +49,7 @@ func mouse_enter(obj):
 				text = text.replace("%1", tr(tt))
 			get_tree().call_group_flags(SceneTree.GROUP_CALL_DEFAULT, "hud", "set_tooltip", text)
 			vm.hover_begin(obj)
-	elif inventory:
+	else:
 		if current_action != "" && current_tool != null:
 			text = tr(current_action + ".combine_id")
 			text = text.replace("%2", tr(tt))
@@ -57,7 +57,7 @@ func mouse_enter(obj):
 		elif obj.inventory:
 			if !current_action:
 				text = tr(tt)
-			else:
+			elif inventory:
 				var action = inventory.get_action()
 				if action == "":
 					action = current_action
