98cb6a2da11a7000c9c03d09b51e770adc49f618
Markus Törnqvist <mjt@fadgames.com>
2018-09-26 13:53:20 +0300

    Drop the requirement that confirm popup must have animations

--- device/ui/confirm_popup.gd
+++ device/ui/confirm_popup.gd
@@ -2,53 +2,74 @@ extends Control
 
 var target = null
 var slot = ""
+var confirm
 var anim
 
 func start(message, p_target, p_slot):
+	target = p_target
+	slot = p_slot
+
 	if has_node("message"):
 		$"message".set_text(message)
 	else:
 		# This allows having TextureRects by the name of eg UI_CONFIRM_NEW_GAME or UI_CONFIRM_QUIT
 		get_node(message).show()
-	target = p_target
-	slot = p_slot
-	anim.play("open")
+
+	if anim:
+		anim.play("open")
+
 	main.menu_open(self)
 	show()
 
 func button_pressed(p_confirm):
-	printt("button pressed!", p_confirm, anim.is_playing())
-	if anim.is_playing():
+	# This must be global or else we might see an animated confirmation menu
+	# overlaid onto the scene when starting a new game. See `anim_finished`.
+	confirm = p_confirm
+
+	printt("button pressed!", slot, target.name, confirm)
+	if anim and anim.is_playing():
+		printt("anim is playing!", anim.current_animation)
 		return
-	if target != null:
-		target.call_deferred(slot, p_confirm)
+
+	# `anim_finished` calls target, but call it now if there are no animations
+	if not anim:
+		target.call_deferred(slot, confirm)
+
 	close()
 
 func menu_collapsed():
 	close()
 
-
 func close():
 	main.menu_close(self)
-	if anim.is_playing():
-		var cur = anim.get_current_animation()
-		if cur == "close":
+	if anim and anim.is_playing():
+		printt("close: anim is playing!", anim.current_animation)
+		if anim.current_animation == "close":
 			return
 
-	anim.play("close")
+	elif anim:
+		# Once this is finished, `anim_finished` will call `queue_free()`
+		# and whatever is defined in the target
+		anim.play("close")
+	else:
+		queue_free()
 
 func anim_finished(anim_name):
+	# print("Finished anim ", anim_name)
 	if anim_name == "close":
+		target.call_deferred(slot, confirm)
 		queue_free()
 
 func input(event):
-	if anim.is_playing():
+	if anim and anim.is_playing():
 		return
 	if event.is_action("menu_request") && event.is_pressed() && !event.is_echo():
 		button_pressed(false)
 
 func _ready():
-	get_node("yes").connect("pressed", self, "button_pressed", [true])
-	get_node("no").connect("pressed", self, "button_pressed", [false])
-	anim = get_node("animation")
-	anim.connect("animation_finished", self, "anim_finished")
+	$"yes".connect("pressed", self, "button_pressed", [true])
+	$"no".connect("pressed", self, "button_pressed", [false])
+	if has_node("animation"):
+		anim = $"animation"
+		anim.connect("animation_finished", self, "anim_finished")
+
