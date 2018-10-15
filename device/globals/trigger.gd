39848f4da5c01f34c0dcb1f7766afa71a730f9dd
santouits <santouits@users.noreply.github.com>
2018-10-15 23:21:29 +0300

    Moved things to clean interactive.gd
    1) Moved everything basic for an interactive object to the interactive.gd and everything else to item.gd that should be cleaned later.
    2) Made trigger.gd extend item.gd instead for now

--- device/globals/trigger.gd
+++ device/globals/trigger.gd
@@ -1,10 +1,4 @@
-extends "res://globals/interactive.gd"
-
-export(String, FILE, ".esc") var events_path
-export var global_id = ""
-export var tooltip = ""
-
-var event_table = {}
+extends "res://globals/item.gd"
 
 func get_tooltip():
 	if TranslationServer.get_locale() == ProjectSettings.get_setting("escoria/platform/development_lang"):
@@ -21,12 +15,6 @@ func get_tooltip():
 		return tooltip_identifier
 
 	return translated
-
-func mouse_enter():
-	get_tree().call_group("game", "mouse_enter", self)
-
-func mouse_exit():
-	get_tree().call_group("game", "mouse_exit", self)
 
 func area_input(viewport, event, shape_idx):
 	input(event)
@@ -59,34 +47,7 @@ func run_event(event):
 	if event in event_table:
 		vm.run_event(event_table[event])
 
-func set_active(p_active):
-	self.visible = p_active
-
 func _ready():
-	var area
-	if has_node("area"):
-		area = get_node("area")
-	else:
-		area = self
-
-	if ClassDB.class_has_signal(area.get_class(), "input_event"):
-		area.connect("input_event", self, "area_input")
-	elif ClassDB.class_has_signal(area.get_class(), "gui_input"):
-		area.connect("gui_input", self, "input")
-	else:
-		vm.report_errors("trigger", ["No input events possible for global_id " + global_id])
-
-	if events_path:
-		var f = File.new()
-		if f.file_exists(events_path):
-			event_table = vm.compile(events_path)
-
-	if ClassDB.class_has_signal(area.get_class(), "mouse_entered"):
-		area.connect("mouse_entered", self, "mouse_enter")
-		area.connect("mouse_exited", self, "mouse_exit")
-
-	vm.register_object(global_id, self)
-
 	connect("body_entered", self, "body_entered")
 	connect("body_exited", self, "body_exited")
 
