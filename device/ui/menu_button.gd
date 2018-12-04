a2bc6f1967666af869eb1bb29a462e515d5d0605
Markus Törnqvist <mjt@fadgames.com>
2018-12-04 18:30:04 +0200

    Make translation_id not be a requirement in menu_button.gd

--- device/ui/menu_button.gd
+++ device/ui/menu_button.gd
@@ -3,8 +3,9 @@ extends Button
 export(String) var translation_id
 
 func language_changed():
-	var ptext = TranslationServer.translate(translation_id)
-	self.text = ptext
+	if translation_id:
+		var ptext = TranslationServer.translate(translation_id)
+		self.text = ptext
 
 func _ready():
 	add_to_group("ui")
