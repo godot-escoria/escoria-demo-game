57b3e18ede6cbbc993c40956bd8bb184a3077f7e
Markus Törnqvist <mjt@fadgames.com>
2018-12-12 09:05:13 +0200

    Compile event table on scene so you can schedule events on _scene instead of misc objects

--- device/globals/scene_base.gd
+++ device/globals/scene_base.gd
@@ -3,6 +3,8 @@ extends Node
 export(String, FILE, ".esc") var events_path = ""
 export(float) var default_zoom = 1.0
 
+var event_table
+
 func _ready():
 	# Call `set_current_scene` only when not loading the game, because doing so
 	# when loading the game causes glitches in the events when `set_scene` also
@@ -10,4 +12,7 @@ func _ready():
 	var run_events = not vm.loading_game
 	if run_events:
 		main.call_deferred("set_current_scene", self, run_events)
+
+	if events_path:
+		event_table = vm.compile(events_path)
 
