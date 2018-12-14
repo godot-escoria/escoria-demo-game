450474ae7086096d2b44d1afad82d83b7b198872
Markus Törnqvist <mjt@fadgames.com>
2018-12-14 16:24:49 +0200

    Implement get_camera_pos() for target.gd

--- device/globals/target.gd
+++ device/globals/target.gd
@@ -2,9 +2,20 @@ extends Position2D
 
 export(String) var global_id
 
+var camera_pos
+
 func get_interact_pos():
 	return get_global_position()
 
+func get_camera_pos():
+	if camera_pos:
+		return camera_pos.get_global_position()
+
+	return get_global_position()
+
 func _ready():
+	if has_node("camera_pos"):
+		camera_pos = $"camera_pos"
+
 	vm.register_object(global_id, self)
 
