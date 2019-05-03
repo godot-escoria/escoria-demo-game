01424debf91da910b33dea3945d21235242393fe
Markus Törnqvist <mjt@fadgames.com>
2019-05-03 16:44:58 +0300

    Fix a flip bug in static_shadow_sprite.gd

--- device/globals/static_shadow_sprite.gd
+++ device/globals/static_shadow_sprite.gd
@@ -2,8 +2,14 @@ extends Sprite
 
 onready var character = get_parent()
 
+var prev_scale_x
+
 func _process(_delta):
 	# Compensate for the character flipping
-	if character.scale.x < 0:
+	if character.scale.x < 0 and character.scale.x != prev_scale_x:
 		self.scale.x *= -1
+		prev_scale_x = character.scale.x
+	elif self.scale.x < 0 and character.scale.x > 0:
+		self.scale.x *= -1
+		prev_scale_x = character.scale.x
 
