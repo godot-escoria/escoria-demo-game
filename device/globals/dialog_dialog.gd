0fe7bb57a0b68bbdeba18014eb891dd25db39e5b
Tyrone Hattingh <tyrone.hattingh@gmail.com>
2020-10-05 07:51:28 +0200

    i variable is being used, cannot use it for loop

--- device/globals/dialog_dialog.gd
+++ device/globals/dialog_dialog.gd
@@ -91,8 +91,8 @@ func start(params, p_context):
 		if params.size() >= 3:
 			avatar = params[2]
 		var avatars = get_node("wrapper/avatars")
-		for i in range(avatars.get_child_count()):
-			var c = avatars.get_child(i)
+		for j in range(avatars.get_child_count()):
+			var c = avatars.get_child(j)
 			if c.get_name() == avatar:
 				c.show()
 			else:
