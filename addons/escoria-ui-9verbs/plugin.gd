dc4cda82ab3d1e30ed3165045f090dfb4aa641fd
Julian Murgia <the.straton@gmail.com>
2021-07-05 14:34:44 +0200

    Moved UI scenes and resources to their own plugin.

---
+++ addons/escoria-ui-9verbs/plugin.gd
@@ -0,0 +1,9 @@
+# Plugin script to initialize Escoria simple mouse UI
+tool
+extends EditorPlugin
+
+
+# Setup Escoria
+func _enter_tree():
+    ProjectSettings.set_setting("escoria/ui/tooltip_follows_mouse", false)
+    ProjectSettings.set_setting("escoria/ui/game_scene", "res://addons/escoria-ui-9verbs/game.tscn")
