3db57a52546949334bd1e1f066ea0874a46af21d
Dennis Ploeger <develop@dieploegers.de>
2021-07-29 21:23:15 +0200

    feat: Implemented turn_to

--- addons/escoria-core/game/core-scripts/esc/commands/turn_to.gd
+++ addons/escoria-core/game/core-scripts/esc/commands/turn_to.gd
@@ -1,4 +1,4 @@
-# `turn_to object degrees`
+# `turn_to object degrees [immediate]`
 # 
 # Turns object to a degrees angle with a directions animation.
 #
@@ -6,6 +6,9 @@
 # When turning to the destination angle, animations are played if they're 
 # defined in animations. object must be player or interactive. degrees must 
 # be between [0, 360] or an error is reported.
+#
+# Set immediate to true to show directly switch to the direction and not
+# show intermediate angles
 #  
 # @STUB
 # @ESC
@@ -17,8 +20,8 @@ class_name TurnToCommand
 func configure() -> ESCCommandArgumentDescriptor:
 	return ESCCommandArgumentDescriptor.new(
 		2, 
-		[TYPE_STRING, TYPE_INT],
-		[null, true]
+		[TYPE_STRING, TYPE_INT, TYPE_BOOL],
+		[null, null, false]
 	)
 	
 
@@ -32,13 +35,22 @@ func validate(arguments: Array):
 			]
 		)
 		return false
+	if arguments[1] < 0 or arguments[1] > 360:
+		escoria.logger.report_errors(
+			"turn_to: invalid degrees",
+			[
+				"Degree %d not between 0 and 360" % arguments[1]
+			]
+		)
+		return false
 	return .validate(arguments)
 
 
 # Run the command
 func run(command_params: Array) -> int:
-	escoria.logger.report_errors(
-		"turn_to: command not implemented",
-		[]
-	)
-	return ESCExecution.RC_ERROR
+	(escoria.object_manager.get_object(command_params[0]).node as ESCItem)\
+		.set_angle(
+			command_params[1],
+			command_params[2]
+		)
+	return ESCExecution.RC_OK
