626e619ae89a5e04572a6d269f0808034ac971b0
Julian Murgia <the.straton@gmail.com>
2021-07-30 13:10:13 +0200

    Turn characters animation script into a resource.

---
+++ addons/escoria-core/game/core-scripts/resources/esc_animationname.gd
@@ -0,0 +1,10 @@
+# Class defining an animation to use for an angle.
+
+extends Resource
+class_name AnimationName
+
+# Name of the animation
+export var animation: String
+
+# Animation mirror (1 is no mirror, -1 is mirrored)
+export var mirrored: int
--- addons/escoria-core/game/core-scripts/resources/esc_animationresource.gd
+++ addons/escoria-core/game/core-scripts/resources/esc_animationresource.gd
@@ -10,20 +10,49 @@ class_name ESCAnimationResource
 # start_angle must be between 0 and 360.
 # Angle 0 and 360 are the same and correspond to UP/NORTH
 # 90 is RIGHT/EAST, 180 is DOWN/SOUTH, etc
-export(Array) var dir_angles = []
+export(Array, Resource) var dir_angles = []
 
 # Array of animations for each direction, from UP to RIGHT_UP clockwise
 # [animation_name, scale]: scale parameter can be set to -1 to mirror 
 # the animation
-export(Array) var directions = []
+export(Array, Resource) var directions = []
 
 
 # Array containing the idle animations for each direction (in the
 # order defined by dir_angles): scale parameter can be set to -1 to mirror 
 # the animation
-export(Array) var  idles = []
+export(Array, Resource) var idles = []
 
 # Array containing the speak animations for each direction (in the
 # order defined by dir_angles): scale parameter can be set to -1 to mirror 
 # the animation
-export(Array) var speaks = []
+export(Array, Resource) var speaks = []
+
+
+func _get_property_list():
+	var properties = []
+	properties.append({
+		"name": "dir_angles",
+		"type": TYPE_ARRAY,
+		"hint": 19,
+		"hint_string": "17/17:DirectionAngle"
+	})
+	properties.append({
+		"name": "directions",
+		"type": TYPE_ARRAY,
+		"hint": 19,
+		"hint_string": "17/17:AnimationName"
+	})
+	properties.append({
+		"name": "idles",
+		"type": TYPE_ARRAY,
+		"hint": 19,
+		"hint_string": "17/17:AnimationName"
+	})
+	properties.append({
+		"name": "speaks",
+		"type": TYPE_ARRAY,
+		"hint": 19,
+		"hint_string": "17/17:AnimationName"
+	})
+	return properties
