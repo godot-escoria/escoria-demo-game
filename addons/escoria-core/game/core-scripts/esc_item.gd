8478e1f67190a421d5d77340251d77757fad74c1
Dennis Ploeger <develop@dieploegers.de>
2021-11-22 14:46:22 +0100

    fix: Safety guard for characters without speaking animations

--- addons/escoria-core/game/core-scripts/esc_item.gd
+++ addons/escoria-core/game/core-scripts/esc_item.gd
@@ -442,7 +442,9 @@ func turn_to(object: Node, wait: float = 0.0):
 
 # Play the talking animation
 func start_talking():
-	if get_animation_player():
+	if get_animation_player() and \
+			_movable.last_dir >= 0 and \
+			_movable.last_dir <= animations.speaks.size():
 		if get_animation_player().is_playing():
 			get_animation_player().stop()
 		get_animation_player().play(
@@ -452,7 +454,9 @@ func start_talking():
 
 # Stop playing the talking animation
 func stop_talking():
-	if get_animation_player():
+	if get_animation_player() and \
+			_movable.last_dir >= 0 and \
+			_movable.last_dir <= animations.idles.size():
 		if get_animation_player().is_playing():
 			get_animation_player().stop()
 		get_animation_player().play(
