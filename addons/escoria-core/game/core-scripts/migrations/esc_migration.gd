b5d5217aa4971769e9ff775e3b151c48fc8f286e
Dennis Ploeger <github.dennis@dieploegers.de>
2021-12-01 15:00:19 +0100

    feat: Support for Escoria and Game migrations (#473)
    Co-authored-by: Dennis Ploeger <develop@dieploegers.de>

---
+++ addons/escoria-core/game/core-scripts/migrations/esc_migration.gd
@@ -0,0 +1,27 @@
+# Base class for all migration version scripts. Extending scripts should be
+# named like the version they migrate the savegame to. (e.g. 1.0.0.gd, 1.0.1.gd)
+extends Object
+class_name ESCMigration
+
+
+var _savegame: ESCSaveGame
+
+
+# Set the savegame
+#
+# #### Parameters
+# - savegame: Savegame to modify
+func set_savegame(savegame: ESCSaveGame):
+	_savegame = savegame
+	
+
+# Get the savegame
+# **Returns** Savegame
+func get_savegame():
+	return _savegame
+	
+
+# Override this function in the version script with
+# the things that need to be applied to the savegame
+func migrate():
+	pass
