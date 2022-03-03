8e6ecb1de0fe6ecff957955b4fe87adfa70c72b5
Balloonpopper <balloonpopper@git.com>
2022-03-04 09:56:42 +1100

    Feat: New graphics for room 8

--- game/rooms/room08/puzzle/10_buttons_puzzle.gd
+++ game/rooms/room08/puzzle/10_buttons_puzzle.gd
@@ -5,21 +5,21 @@ var next_to_be_pressed: int = 1
 
 func _ready():
 	randomize()
-	
+
 	initialize()
 	reset()
 
 	for button in $GridContainer.get_children():
 		button.connect("pressed", self, "_button_pressed", [button])
-	
+
 	escoria.main.current_scene.game.hide_ui()
 	escoria.main.current_scene.hide()
-	
+
 
 func initialize():
 	numbers_array = range(1, 11)
 	numbers_array.shuffle()
-	
+
 func reset():
 	$win_label.hide()
 	next_to_be_pressed = 1
@@ -39,7 +39,7 @@ func _button_pressed(button: Button):
 	else:
 		button.disabled = true
 		next_to_be_pressed += 1
-	
+
 	if next_to_be_pressed == 11:
 		win()
 
@@ -47,11 +47,12 @@ func win():
 	$win_label.show()
 	yield(get_tree().create_timer(2), "timeout")
 	hide()
-	
+
 	escoria.main.current_scene.game.show_ui()
 	escoria.main.current_scene.show()
 	escoria.globals_manager.set_global("r8_m_door_open", true)
 	escoria.object_manager.get_object("r8_m_door").set_state("door_open")
+	escoria.object_manager.get_object("r8_mini_puzzle_button").active = false
 
 
 func _on_quit_pressed():
