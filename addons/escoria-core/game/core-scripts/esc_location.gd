69d3a40da35e8201e7667ad75bbb89a5cfac1ecf
balloonpopper <5151242+balloonpopper@users.noreply.github.com>
2022-03-09 18:26:30 +1100

    Update addons/escoria-core/game/core-scripts/esc_location.gd
    Co-authored-by: Duncan Brown <duncan@prometheussoftware.ca>

--- addons/escoria-core/game/core-scripts/esc_location.gd
+++ addons/escoria-core/game/core-scripts/esc_location.gd
@@ -1,8 +1,8 @@
 # A simple node extending Position2D with a global ID so that it can be
-# referenced in ESC Scripts. Movement-based commands like walk_to_pos will
-# automatically use an ESClocation that is a child of the destination node.
-# Commands like turn_to which are not movement-based will ignore child
-# ESClocations and refer to the parent node.
+# referenced in ESC Scripts. Movement-based commands like `walk_to_pos` will
+# automatically use an `ESCLocation` that is a child of the destination node.
+# Commands like `turn_to`--which are not movement-based--will ignore child
+# `ESCLocation`s and refer to the parent node.
 extends Position2D
 class_name ESCLocation, "res://addons/escoria-core/design/esc_location.svg"
 
