ade537a2736ccfde8e566a767951f652d0991f85
Julian Murgia <the.straton@gmail.com>
2022-03-29 07:49:17 +0200

    Moved the check in inventory_add command

--- addons/escoria-core/game/core-scripts/esc/esc_globals_manager.gd
+++ addons/escoria-core/game/core-scripts/esc/esc_globals_manager.gd
@@ -92,15 +92,7 @@ func set_global(key: String, value, ignore_reserved: bool = false) -> void:
 				"Global key %s is reserved and can not be overridden" % key
 			]
 		)
-	
-	if key.count("/") > 1 and key.substr(1, 1) == "/":
-		escoria.logger.report_errors(
-			"ESCGlobalsManager.set_global: Global name error",
-			[
-				"Global key %s has more than 1 occurrence of '/'" % key
-			]
-		)
-		
+
 	emit_signal(
 		"global_changed",
 		key,
