8dc61cc6132c25897d4d7c655b727ba9995d9219
Balloonpopper <balloonpopper@git.com>
2022-04-04 23:44:52 +1000

    feat: Added print command to print output at any debug level

---
+++ addons/escoria-core/game/core-scripts/esc/commands/print.gd
@@ -0,0 +1,27 @@
+# `debug string [string2 ...]`
+#
+# Prints a message to the Godot debug window. 
+# Use this for debugging game state.
+#
+# **Parameters**
+#
+# - *string*: The string to log
+#
+# @ESC
+extends ESCBaseCommand
+class_name PrintCommand
+
+
+# Return the descriptor of the arguments of this command
+func configure() -> ESCCommandArgumentDescriptor:
+	return ESCCommandArgumentDescriptor.new(
+		1,
+		[TYPE_STRING],
+		[""]
+	)
+
+
+# Run the command
+func run(command_params: Array) -> int:
+	print(command_params[0])
+	return ESCExecution.RC_OK
