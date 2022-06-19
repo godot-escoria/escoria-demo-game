9d819e0609673bdc84bdd0ad6b9790cae9374300
Balloonpopper <balloonpopper@git.com>
2022-06-19 23:37:31 +1000

    Tooltips. Progress bar. Post-export information window.

--- addons/escoria-player-creator/draw_frame_rects.gd
+++ addons/escoria-player-creator/draw_frame_rects.gd
@@ -1,12 +1,19 @@
 tool
 extends Control
 
+# Number of rows to break the spritesheet into
 export var total_num_rows:int
+# Number of columns to break the spritesheet into
 export var total_num_columns:int
+# First animation cell. Boxes will be drawn around every cell from the start_cell to end_cell.
 export var start_cell:int
+# Last animation cell. Boxes will be drawn around every cell from the start_cell to end_cell.
 export var end_cell:int
+# The size of each animation cell in pixels.
 export var cell_size:Vector2
 
+# This function will calculate where on the spritesheet the start and end frames for an animation
+# are, and draw boxes around all used frames. The visual indicator makes frame selection easier.
 func _draw() -> void:
 	for loop in range(end_cell - start_cell + 1):
 
@@ -19,4 +26,3 @@ func _draw() -> void:
 		draw_line(Vector2(corner1.x, corner1.y), Vector2(corner1.x, corner2.y), Color(255,0,255), 2.0, false)
 		draw_line(Vector2(corner1.x, corner2.y), Vector2(corner2.x, corner2.y), Color(255,0,255), 2.0, false)
 		draw_line(Vector2(corner2.x, corner1.y), Vector2(corner2.x, corner2.y), Color(255,0,255), 2.0, false)
-#		print("Current = "+str(current_cell)+", row="+str(frame_row)+", col="+str(frame_column)+", corner1 = "+str(corner1)+", corner2="+str(corner2))
--- addons/escoria-player-creator/plugin.gd
+++ addons/escoria-player-creator/plugin.gd
@@ -3,35 +3,38 @@ extends EditorPlugin
 
 const helper_ui = preload("res://addons/escoria-player-creator/escoria_player_creator.tscn")
 
+# This is the instance of the plugin code that is instantiated by the plugin.
 var helper_instance
 
 func _enter_tree() -> void:
-	#helper_instance = helper_ui.instance()
 	helper_instance = helper_ui.instance()
 	helper_instance.plugin_reference = self
 	# Add the panel to the main viewport
 	get_editor_interface().get_editor_viewport().add_child(helper_instance)
-	# Hide the interface panel
-	#make_visible(false)
 
 
 func _exit_tree() -> void:
 	if helper_instance:
 		helper_instance.queue_free()
 
+
 func has_main_screen() -> bool:
 	# Add the button to the Godot interface ribbon
 	return true
+
 
 func make_visible(visible: bool) -> void:
 	if helper_instance:
 		helper_instance.visible = visible
 
+
 func get_plugin_name() -> String:
 	return "Escoria Player Creator"
 
+
 func get_plugin_icon() -> Texture:
 	return (preload("res://addons/escoria-player-creator/graphics/icon16x16.png"))
+
 
 func open_scene(path: String) -> void:
 	get_editor_interface().open_scene_from_path(path)
