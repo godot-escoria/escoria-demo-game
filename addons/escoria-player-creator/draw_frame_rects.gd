257320482703a5ac89c63cf2c03670a785efff39
Balloonpopper <balloonpopper@git.com>
2022-06-28 13:30:40 +1000

    Fix issues with box drawing by scaling the pixel coords rather than the control node

--- addons/escoria-player-creator/draw_frame_rects.gd
+++ addons/escoria-player-creator/draw_frame_rects.gd
@@ -11,6 +11,8 @@ export var start_cell:int
 export var end_cell:int
 # The size of each animation cell in pixels.
 export var cell_size:Vector2
+# Zoom factor
+export var zoom_factor:float
 
 # This function will calculate where on the spritesheet the start and end frames for an animation
 # are, and draw boxes around all used frames. The visual indicator makes frame selection easier.
@@ -22,7 +24,9 @@ func _draw() -> void:
 		var frame_column = (current_cell - 1) % total_num_columns
 		var corner1 = Vector2(frame_column * cell_size.x, frame_row * cell_size.y)
 		var corner2 = Vector2(corner1.x + cell_size.x, corner1.y + cell_size.y)
-		draw_line(Vector2(corner1.x, corner1.y), Vector2(corner2.x, corner1.y), Color(255,0,255), 2.0, false)
-		draw_line(Vector2(corner1.x, corner1.y), Vector2(corner1.x, corner2.y), Color(255,0,255), 2.0, false)
-		draw_line(Vector2(corner1.x, corner2.y), Vector2(corner2.x, corner2.y), Color(255,0,255), 2.0, false)
-		draw_line(Vector2(corner2.x, corner1.y), Vector2(corner2.x, corner2.y), Color(255,0,255), 2.0, false)
+		draw_line(Vector2(corner1.x * zoom_factor, corner1.y * zoom_factor), Vector2(corner2.x * zoom_factor, corner1.y * zoom_factor), Color(255,0,255), 2.0, false)
+		draw_line(Vector2(corner1.x * zoom_factor, corner1.y * zoom_factor), Vector2(corner1.x * zoom_factor, corner2.y * zoom_factor), Color(255,0,255), 2.0, false)
+		draw_line(Vector2(corner1.x * zoom_factor, corner2.y * zoom_factor), Vector2(corner2.x * zoom_factor, corner2.y * zoom_factor), Color(255,0,255), 2.0, false)
+		draw_line(Vector2(corner2.x * zoom_factor, corner1.y * zoom_factor), Vector2(corner2.x * zoom_factor, corner2.y * zoom_factor), Color(255,0,255), 2.0, false)
+
+
