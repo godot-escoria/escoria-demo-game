4a31315be1019d470978afa1a049f2d8d512577f
skogach <skogach@gmail.com>
2023-02-14 10:27:06 +0300

    move dialog settings to another script to fix exported game issues

---
+++ addons/escoria-dialog-simple/esc_dialo_simple_settings.gd
@@ -0,0 +1,20 @@
+extends Resource
+class_name ESCSimpleDialogSettingsManager
+
+const SETTINGS_ROOT="escoria/dialog_simple"
+
+const AVATARS_PATH = "%s/avatars_path" % SETTINGS_ROOT
+const TEXT_TIME_PER_LETTER_MS = "%s/text_time_per_letter_ms" % SETTINGS_ROOT
+const TEXT_TIME_PER_LETTER_MS_FAST = "%s/text_time_per_fast_letter_ms" % SETTINGS_ROOT
+const READING_SPEED_IN_WPM = "%s/reading_speed_in_wpm" % SETTINGS_ROOT
+const CLEAR_TEXT_BY_CLICK_ONLY = "%s/clear_text_by_click_only" % SETTINGS_ROOT
+const LEFT_CLICK_ACTION = "%s/left_click_action" % SETTINGS_ROOT
+
+const STOP_TALKING_ANIMATION_ON = "%s/stop_talking_animation_on" % SETTINGS_ROOT
+
+const LEFT_CLICK_ACTION_SPEED_UP = "Speed up"
+const LEFT_CLICK_ACTION_INSTANT_FINISH = "Instant finish"
+const LEFT_CLICK_ACTION_NOTHING = "None"
+
+const STOP_TALKING_ANIMATION_ON_END_OF_TEXT = "End of text"
+const STOP_TALKING_ANIMATION_ON_END_OF_AUDIO = "End of audio"
