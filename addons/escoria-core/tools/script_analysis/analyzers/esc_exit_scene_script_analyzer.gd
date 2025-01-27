7e51f5ed6a2d9828f2e71c37992ad13ba94f20c7
Duncan Brown <duncan@prometheussoftware.ca>
2025-01-26 19:19:32 -0500

    feat: adds check for commands after change_scene as they won't be executed; also some refactoring and improvements in warning message rendering and formatting

--- addons/escoria-core/tools/script_analysis/analyzers/esc_exit_scene_script_analyzer.gd
+++ addons/escoria-core/tools/script_analysis/analyzers/esc_exit_scene_script_analyzer.gd
@@ -7,10 +7,14 @@ const ACCEPT_INPUT_DISABLE_ARGS = ["none", "skip"]
 
 const CHANGE_SCENE_MISSING_MESSAGE = "Event ':exit_scene' is missing 'change_scene' command. Scene might not change as expected. Ignore this warning if the command was omitted on purpose."
 
+const COMMANDS_AFTER_CHANGE_SCENE_MESSAGE = "Event ':exit_scene' may have commands that are expected to be run after a call to 'change_scene'. Such commands will not be executed."
+
 const MISSING_ACCEPT_INPUT_MESSAGE = \
 	"Event ':exit_scene' may allow for the player character to move while exiting and changing the scene." \
 	+ " To prevent this, ensure a call to 'accept_input' with an argument evaluating to 'NONE' or 'SKIP' is" \
 	+ " made before any calls to 'transition' and/or 'change_scene'."
+
+const BULLET_CHARACTER = "- "
 
 
 # These are vars because they have to be, but should be treated as consts
@@ -37,13 +41,25 @@ func analyze(statements: Array) -> void:
 		if statement.get_event_name().to_lower() == EXIT_SCENE_EVENT_NAME:
 			_execute(statement)
 
-			if not _has_change_scene_command:
-				_rich_messages.append(ESCSafeLogging.log_warn.bind(self, CHANGE_SCENE_MISSING_MESSAGE))
-
-			if _accept_input_disable_missing:
-				_rich_messages.append(ESCSafeLogging.log_warn.bind(self, MISSING_ACCEPT_INPUT_MESSAGE))
+			_add_warning_messages()
 
 			break
+
+
+func _add_warning_messages() -> void:
+	var messages: Array[String] = []
+
+	if not _has_change_scene_command:
+		messages.append(BULLET_CHARACTER + CHANGE_SCENE_MISSING_MESSAGE)
+
+	if _accept_input_disable_missing:
+		messages.append(BULLET_CHARACTER + MISSING_ACCEPT_INPUT_MESSAGE)
+
+	if _has_commands_after_change_scene_command:
+		messages.append(BULLET_CHARACTER + COMMANDS_AFTER_CHANGE_SCENE_MESSAGE)
+
+	if not messages.is_empty():
+		_rich_messages.append(ESCSafeLogging.log_warn.bind(self, "\n".join(messages)))
 
 
 func _execute_block(statements: Array, env: ESCEnvironment):
@@ -76,7 +92,11 @@ func visit_call_expr(expr: ESCGrammarExprs.Call):
 
 		args.append(arg)
 
-	if callee.get_command_name() == CHANGE_SCENE_COMMAND_NAME:
+	var command_name: String = _get_command_name(callee)
+
+	_has_commands_after_change_scene_command = _has_change_scene_command_in_scope()
+
+	if command_name == CHANGE_SCENE_COMMAND_NAME:
 		_has_change_scene_command = true
 
 		_change_scene_token = expr.get_paren_token()
@@ -89,9 +109,9 @@ func visit_call_expr(expr: ESCGrammarExprs.Call):
 
 			if is_auto_transition != null and not is_auto_transition:
 				_check_for_missing_accept_input_disable()
-	elif callee.get_command_name() == TRANSITION_COMMAND_NAME:
+	elif command_name == TRANSITION_COMMAND_NAME:
 		_check_for_missing_accept_input_disable()
-	elif callee.get_command_name() == ACCEPT_INPUT_COMMAND_NAME:
+	elif command_name == ACCEPT_INPUT_COMMAND_NAME:
 		var first_arg = args[0] as String
 
 		if first_arg != null and first_arg.to_lower() in ACCEPT_INPUT_DISABLE_ARGS:
@@ -113,9 +133,19 @@ func _check_for_missing_accept_input_disable() -> void:
 		or not _environment.get_value(_accept_input_token)
 
 
-#func _check_for_commands_after_change_scene_command() -> void:
-	#if _change_scene_token == null:
-		#return
-#
-	#if not _environment.is_valid_key(_accept_input_token) or not _environment.get_value(_accept_input_token):
-		#_accept_input_disable_missing = true
+func _has_change_scene_command_in_scope() -> bool:
+	if _change_scene_token == null:
+		return false
+
+	return \
+		_environment.is_valid_key(_change_scene_token) \
+		and _environment.get_value(_change_scene_token)
+
+
+func _get_command_name(callee) -> String:
+	# if we don't have a command to run, check against any built-in functions and,
+	# if one is found and is executed, we're done
+	if callee is ESCBaseCommand:
+		return callee.get_command_name()
+
+	return callee
