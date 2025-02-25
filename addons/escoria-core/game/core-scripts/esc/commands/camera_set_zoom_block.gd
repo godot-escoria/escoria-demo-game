1824785e2af896c5c5e94cb725946e57509051d2
Duncan Brown <duncan@prometheussoftware.ca>
2025-02-25 18:44:16 -0500

    chore: updates all commands to use Godot 4 docstrings for public interfaces

--- addons/escoria-core/game/core-scripts/esc/commands/camera_set_zoom_block.gd
+++ addons/escoria-core/game/core-scripts/esc/commands/camera_set_zoom_block.gd
@@ -1,24 +1,24 @@
-# `camera_set_zoom_block magnitude [time]`
-#
-# Zooms the camera in/out to the desired `magnitude`. Values larger than '1' zoom
-# the camera out while smaller values zoom in. These values are relative to the
-# default zoom value of '1', not the current value. As such, while using a value
-# of '0.5' would double the size of the graphics, running the same command again
-# would result in no change. The zoom will happen over the given time period.
-# Blocks until the command completes.
-#
-# Zoom operations might not be as smooth as desired if the requested zoom
-# level results in an edge of the camera meeting any defined camera limits.
-#
-# **Parameters**
-#
-# - *magnitude*: Magnitude of zoom
-# - *time*: Number of seconds the transition should take, with a value of `0`
-#   meaning the zoom should happen instantly (default: `0`)
-#
-# For more details see: https://docs.escoria-framework.org/camera
-#
-# @ESC
+## `camera_set_zoom_block magnitude [time]`
+##
+## Zooms the camera in/out to the desired `magnitude`. Values larger than '1' zoom
+## the camera out while smaller values zoom in. These values are relative to the
+## default zoom value of '1', not the current value. As such, while using a value
+## of '0.5' would double the size of the graphics, running the same command again
+## would result in no change. The zoom will happen over the given time period.
+## Blocks until the command completes.[br]
+##[br]
+## Zoom operations might not be as smooth as desired if the requested zoom
+## level results in an edge of the camera meeting any defined camera limits.[br]
+##[br]
+## **Parameters**[br]
+##[br]
+## - *magnitude*: Magnitude of zoom[br]
+## - *time*: Number of seconds the transition should take, with a value of `0`
+##   meaning the zoom should happen instantly (default: `0`)[br]
+##[br]
+## For more details see: https://docs.escoria-framework.org/camera
+##
+## @ESC
 extends ESCCameraBaseCommand
 class_name CameraSetZoomBlockCommand
 
@@ -26,7 +26,7 @@ class_name CameraSetZoomBlockCommand
 var _camera_tween: Tween3
 
 
-# Return the descriptor of the arguments of this command
+## Return the descriptor of the arguments of this command
 func configure() -> ESCCommandArgumentDescriptor:
 	return ESCCommandArgumentDescriptor.new(
 		1,
@@ -35,7 +35,7 @@ func configure() -> ESCCommandArgumentDescriptor:
 	)
 
 
-# Validate whether the given arguments match the command descriptor
+## Validate whether the given arguments match the command descriptor
 func validate(arguments: Array):
 	if not super.validate(arguments):
 		return false
@@ -46,7 +46,7 @@ func validate(arguments: Array):
 	return true
 
 
-# Run the command
+## Run the command
 func run(command_params: Array) -> int:
 	var camera: ESCCamera = escoria.object_manager.get_object(escoria.object_manager.CAMERA).node as ESCCamera
 
@@ -61,7 +61,7 @@ func run(command_params: Array) -> int:
 	return ESCExecution.RC_OK
 
 
-# Function called when the command is interrupted.
+## Function called when the command is interrupted.
 func interrupt():
 	escoria.logger.debug(
 		self,
