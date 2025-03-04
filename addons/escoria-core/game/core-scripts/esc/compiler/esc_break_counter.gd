7cc383212366ee701ae733f9841dae83818de5db
Duncan Brown <duncan@prometheussoftware.ca>
2025-03-04 17:28:48 -0500

    chore(wip): updates to Godot 4 docstrings

--- addons/escoria-core/game/core-scripts/esc/compiler/esc_break_counter.gd
+++ addons/escoria-core/game/core-scripts/esc/compiler/esc_break_counter.gd
@@ -1,3 +1,4 @@
+## Used by the intpreter to track the depth of a `break` statement invoked in a dialog.
 extends Object
 class_name ESCBreakCounter
 
@@ -7,13 +8,16 @@ var _levels_left: int = 0:
 	get = get_levels_left
 
 
+## Sets the depth from the root of the dialog tree, with `levels == 0` at the root.
 func set_levels_left(levels: int) -> void:
 	_levels_left = levels
 
 
+## Gets the depth from the root of the dialog tree, with `levels == 0` at the root.
 func get_levels_left() -> int:
 	return _levels_left
 
 
+## Decrements by one the depth from the root of the dialog tree, with `levels == 0` at the root.
 func dec_levels_left() -> void:
 	_levels_left = _levels_left - 1
