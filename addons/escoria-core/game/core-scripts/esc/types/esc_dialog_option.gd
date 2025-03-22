2d97db49e27f2d7edfcdf0c5f32a439babf0aba0
Duncan Brown <duncan@prometheussoftware.ca>
2025-03-22 11:28:06 -0400

    wip: removes dead code

--- addons/escoria-core/game/core-scripts/esc/types/esc_dialog_option.gd
+++ addons/escoria-core/game/core-scripts/esc/types/esc_dialog_option.gd
@@ -3,12 +3,6 @@ extends ESCStatement
 class_name ESCDialogOption
 
 
-# Regex that matches dialog option lines
-const REGEX = \
-	'^[^-]*- (?<trans_key>[^:]+)?:?"' +\
-	'(?<option>[^"]+)"( \\[(?<conditions>[^\\]]+)\\])?$'
-
-
 # Option displayed in the HUD
 var option: String:
 	get = get_translated_option
@@ -16,46 +10,9 @@ var option: String:
 # Maps back to the parsed source option.
 var source_option
 
-# Conditions to show this dialog
-var conditions: Array = []
-
 var _is_valid: bool:
 	set = set_is_valid,
 	get = is_valid
-
-# Create a dialog option from an ESC string
-#
-# #### Parameter
-# - option_string: ESC string for the dialog option
-func load_string(option_string: String):
-	var option_regex = RegEx.new()
-	option_regex.compile(REGEX)
-
-	if option_regex.search(option_string):
-		for result in option_regex.search_all(option_string):
-			if "option" in result.names:
-				var _trans_key = ""
-				if "trans_key" in result.names:
-					_trans_key = "%s:" % \
-							ESCUtils.get_re_group(result, "trans_key")
-				self.option = "%s%s" % [
-					_trans_key,
-					ESCUtils.get_re_group(result, "option")
-				]
-			if "conditions" in result.names:
-				for condition_text in ESCUtils.get_re_group(
-							result,
-							"conditions"
-						).split(","):
-					self.conditions.append(
-						ESCCondition.new(condition_text.strip_edges())
-					)
-	else:
-		escoria.logger.error(
-			self,
-			"Invalid dialog option detected: %s." % option_string +
-				"Dialog option regexp didn't match"
-		)
 
 
 func get_translated_option():
@@ -76,10 +33,6 @@ func get_translated_option():
 
 # Check, if conditions match
 func is_valid() -> bool:
-#	for condition in self.conditions:
-#		if not (condition as ESCCondition).run():
-#			return false
-#	return true
 	return _is_valid
 
 
