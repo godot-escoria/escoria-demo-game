ccd20ec462baef30839af92a31fa86c626724989
Duncan Brown <duncan@prometheussoftware.ca>
2025-03-28 12:33:21 -0400

    wip: updates to use Godot 4-style docstrings; also currently looking at ESCStatement and its derivatives to see how much they're still used/needed

--- addons/escoria-core/game/core-scripts/esc/types/esc_execution.gd
+++ addons/escoria-core/game/core-scripts/esc/types/esc_execution.gd
@@ -1,14 +1,15 @@
-# Basic features and informations about ESC executions
+## Class with enum containing the return codes returned by ASHES script functions.
 extends Resource
 class_name ESCExecution
 
 
-# Return codes handled by events
-# * RC_OK: Event run okay
-# * RC_CANCEL: Cancel all scheduled and queued events. This return code tells the Event Manager
-# that no execution is required for this command (such as "stop" and "repeat")
-# * RC_ERROR: Error running a command
-# * RC_REPEAT: Repeat the current scope from the beginning
-# * RC_INTERRUPTED: Event was interrupted
-# * RC_WONT_QUEUE: Event won't or can't be queued
+## Return codes handled by events:[br]
+##[br]
+## * RC_OK: Event run okay.[br]
+## * RC_CANCEL: Cancel all scheduled and queued events. This return code tells the Event Manager
+## that no execution is required for this command (such as "stop" and "repeat").[br]
+## * RC_ERROR: Error running a command.[br]
+## * RC_REPEAT: Repeat the current scope from the beginning.[br]
+## * RC_INTERRUPTED: Event was interrupted.[br]
+## * RC_WONT_QUEUE: Event won't or can't be queued.
 enum {RC_OK, RC_CANCEL, RC_ERROR, RC_REPEAT, RC_INTERRUPTED, RC_WONT_QUEUE}
--- addons/escoria-core/game/core-scripts/esc/types/esc_script.gd
+++ addons/escoria-core/game/core-scripts/esc/types/esc_script.gd
@@ -1,19 +1,11 @@
-# A compiled ESC script
+## Represents a compiled ASHES script.
 extends Resource
 class_name ESCScript
 
 
-# A dictionary of ESCEvents in this script
+## A dictionary of `ESCEvent`s contained in this script.
 var events: Dictionary
 
-
-##############
-# New interpreter stuff
-var parsed_events = {}
-
-
-# The name of the ESC file parsed for this script, if this script was in fact
-# loaded from a file (as opposed to being from an internal string).
+## The name of the ASHES file parsed for this script, if this script was in fact
+## loaded from a file (as opposed to being from an internal string).
 var filename: String = ""
-
-##############
