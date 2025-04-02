a64f6f71803f5462c2c4357bc054b1db4a5255c7
Oier Bravo Urtasun <oierbravo@gmail.com>
2025-04-02 11:08:19 +0200

    Update doc addons/escoria-core/game/esc_dependency_injector.gd
    Co-authored-by: Duncan Brown <duncan@prometheussoftware.ca>

--- addons/escoria-core/game/esc_dependency_injector.gd
+++ addons/escoria-core/game/esc_dependency_injector.gd
@@ -1,13 +1,13 @@
-# Escoria dependency injector
-# Holds class creation methods for replacing non singelton Escoria core classes.
-# If you need to replace an Escoria core class follow the next steps:
-# 1. Create a class in your project that extends ESCDepencyInjector: `extends ESCDependencyInjector`
-# 2. Override the methods that create the classes that you want to replace
-# 3. In your game initialitation replace `escoria.di` variable with your class: `escoria.di = MyDependencyInjector.new()`
-#
-# For more details see: https://docs.godotengine.org/en/stable/tutorials/best_practices/scene_organization.html
-#
-# @ESC
+## Escoria dependency injector
+##
+## Holds factory methods for replacing non-singleton Escoria core classes. 
+## If you wish to replace any Escoria core classes with your own derived classes, follow these steps:[br]
+## 1. Create a class in your project that extends `ESCDependencyInjector`: `extends ESCDependencyInjector`[br]
+## 2. Override the methods that create the classes that you want to replace.[br]
+## 3. In your game initialization, replace the `escoria.di` instance variable with your new dependency injector: 
+## `escoria.di = MyDependencyInjector.new()`[br]
+##[br]
+## For more details see: https://docs.godotengine.org/en/stable/tutorials/best_practices/scene_organization.html
 class_name ESCDependencyInjector
 
 func esc_inventory_item(p_item: ESCItem):
