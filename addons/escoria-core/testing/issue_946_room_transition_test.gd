42c5ea5e8e21ae522d68ffbc3a71dc1a995c5a48
Duncan Brown <duncan@prometheussoftware.ca>
2026-05-04 13:50:21 -0400

    test(issue-946): bound transition event wait

--- addons/escoria-core/testing/issue_946_room_transition_test.gd
+++ addons/escoria-core/testing/issue_946_room_transition_test.gd
@@ -5,9 +5,12 @@ extends GdUnitTestSuite
 const MARK_SCRIPT := "res://game/characters/mark/mark.esc"
 const ROOM_05 := "res://game/rooms/room05/room05.tscn"
 const ROOM_06 := "res://game/rooms/room06/room06.tscn"
+const EVENT_FINISHED_TIMEOUT_SECONDS := 5.0
 
 
 var _compiler_before: ESCCompiler
+var _event_finished_rc: Variant = []
+var _event_finished_timed_out := false
 
 
 class CountingCompiler extends ESCCompiler:
@@ -63,12 +66,37 @@ func _change_scene_and_wait(room_path: String, enable_automatic_transition: bool
 
 
 func _wait_for_event_finished(event_name: String) -> void:
-	for attempt in 60:
-		var rc = await escoria.event_manager.event_finished
-		if rc[1] == event_name:
+	_event_finished_rc = []
+	_event_finished_timed_out = false
+
+	escoria.event_manager.event_finished.connect(_on_wait_event_finished)
+	var timeout_timer := get_tree().create_timer(EVENT_FINISHED_TIMEOUT_SECONDS)
+	timeout_timer.timeout.connect(_on_wait_event_finished_timeout)
+
+	while not _event_finished_timed_out:
+		await get_tree().process_frame
+		var rc := _event_finished_rc
+		if rc is Array and rc.size() > 1 and rc[1] == event_name:
+			escoria.event_manager.event_finished.disconnect(_on_wait_event_finished)
+			if timeout_timer.timeout.is_connected(_on_wait_event_finished_timeout):
+				timeout_timer.timeout.disconnect(_on_wait_event_finished_timeout)
 			return
 
-	assert_str("").is_equal(event_name)
+	if escoria.event_manager.event_finished.is_connected(_on_wait_event_finished):
+		escoria.event_manager.event_finished.disconnect(_on_wait_event_finished)
+
+	fail(
+		"Timed out waiting for escoria.event_manager.event_finished for event '%s'."
+				% event_name
+	)
+
+
+func _on_wait_event_finished(return_code, finished_event_name) -> void:
+	_event_finished_rc = [return_code, finished_event_name]
+
+
+func _on_wait_event_finished_timeout() -> void:
+	_event_finished_timed_out = true
 
 
 func _wait_for_frames(count: int) -> void:
